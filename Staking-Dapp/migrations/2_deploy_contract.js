d80b03e5f135279eed12318ea0888e8d0f9607ae
a daana <notaliald1@gmail.com>
2021-10-12 22:47:13 +0300

    Finished bank migration setup

--- Staking-Dapp/migrations/2_deploy_contract.js
+++ Staking-Dapp/migrations/2_deploy_contract.js
@@ -3,7 +3,7 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   // Deploy Mock Tether Token
   await deployer.deploy(Tether);
   const tether = await Tether.deployed();
@@ -12,7 +12,13 @@ module.exports = async function(deployer) {
   await deployer.deploy(RWD);
   const rwd = await RWD.deployed();
 
-  // Deploy DecentralBank
-  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  // Deploy DecentralBank with reward token and tether token
+  await deployer.deploy(DecentralBank);
   const decentralBank = await DecentralBank.deployed();
+
+  // transfer 1 million (all) reward tokens to the bank on migration
+  await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+
+  // Transfer 100 Mock Tether tokens to the second account in the test network
+  await tether.transfer(accounts[1], "100000000000000000000");
 };
--- Staking-Dapp/src/contracts/DecentralBank.sol
+++ Staking-Dapp/src/contracts/DecentralBank.sol
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+// importing other contracts
+import './RWD.sol';
+import './Tether.sol';
+
+
 
 
 contract DecentralBank {
+    //setup variables for the bank
     string public name = 'Decentral Bank';
     address public owner;
+
+    Tether public tether;
+    RWD public rwd;
+
+    // the constructor takes reward and tehter token and set arguments to the contracts
+    constructor(RWD _rwd, Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
+        owner = msg.sender;
+    }
 
 
 }
