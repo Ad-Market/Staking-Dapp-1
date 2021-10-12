0f3ab72956f1287c99272fb59e59d004a917f7cf
a daana <notaliald1@gmail.com>
2021-10-12 22:32:59 +0300

    Finished reward token contract and migration for it

--- Staking-Dapp/migrations/2_deploy_contract.js
+++ Staking-Dapp/migrations/2_deploy_contract.js
@@ -1,6 +1,18 @@
+// grab our contract
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function(deployer) {
+  // Deploy Mock Tether Token
   await deployer.deploy(Tether);
   const tether = await Tether.deployed();
+
+  // Deploy RWD Token
+  await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
+
+  // Deploy DecentralBank
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed();
 };
--- Staking-Dapp/src/contracts/RWD.sol
+++ Staking-Dapp/src/contracts/RWD.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 // creating a Mock tether Token to use
-contract Tether{
+contract RWD{
     
-    string  public name = "Mock Tether Token";// Token name
-    string  public symbol = "mUSDT";// Token icon
+    string  public name = "Reward Token";// Token name
+    string  public symbol = "RWD";// Token icon
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens 
     uint8   public decimals = 18;// how many decimal places
 
