651d9d0a4b756e1dadb55f44e4e4f72fcb96ce7c
a daana <notaliald1@gmail.com>
2021-10-12 21:35:42 +0300

    Finished transfer for mock token

--- Staking-Dapp/src/contracts/Tether.sol
+++ Staking-Dapp/src/contracts/Tether.sol
@@ -7,4 +7,36 @@ contract Tether{
     string  public symbol = "mUSDT";// Token icon
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens 
     uint8   public decimals = 18;// how many decimal places
+
+    // event to transfer from one account to another
+    event Transfer(
+        address indexed _from,
+        address indexed _to, 
+        uint _value
+    );
+    // event to approve transfer
+    // aproval has to come ffrom the owner always
+    event Approval(
+        address indexed _owner,
+        address indexed _spender, 
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() public {
+        // owner has the total supply
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // require that the value is greater or equal to the amount for transfer
+        require(balanceOf[msg.sender] >= _value);
+        // transfer the amount and subtract the balance from who is currently using the bank
+        balanceOf[msg.sender] -= _value;
+        // add the balance to the reciever balance in the mapping
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
