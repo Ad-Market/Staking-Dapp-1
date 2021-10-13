e13433659f1fa0ee0edf95c8772fec90b7642598
a daana <notaliald1@gmail.com>
2021-10-13 20:40:13 +0300

    Navbar setup

--- Staking-Dapp/src/components/App.js
+++ Staking-Dapp/src/components/App.js
@@ -1,13 +1,17 @@
 // main applications where to render all the componenets
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 // create class for component
 class App extends Component {
   render() {
     return (
-      <div className="text-center" style={{}}>
-        <h1>Hello, World</h1>
+      <div>
+        <Navbar />
+        <div className="text-center">
+          <h1>Hello, World</h1>
+        </div>
       </div>
     );
   }
---
+++ Staking-Dapp/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import bank from "../bank.png";
+
+// create class for component
+class Navbar extends Component {
+  render() {
+    return (
+      <nav
+        className="navbar navbar-dark fixed-top shadow p-0"
+        style={{ backgroundColor: "black", height: "50px" }}
+      >
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          style={{ color: "white" }}
+        >
+          <img
+            alt="bank logo"
+            src={bank}
+            width="50"
+            height="35"
+            className="d-inline-block align-top"
+          />
+          &ensp; Dapp Staking Bank
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
+            <small style={{ color: "white" }}>Account Number</small>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
