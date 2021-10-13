99d8711fc48abf7dc0fc7e82a50549ec6daa8e55
a daana <notaliald1@gmail.com>
2021-10-13 20:05:58 +0300

    setting up React

---
+++ Staking-Dapp/src/components/App.js
@@ -0,0 +1,16 @@
+// main applications where to render all the componenets
+import React, { Component } from "react";
+import "./App.css";
+
+// create class for component
+class App extends Component {
+  render() {
+    return (
+      <div className="text-center" style={{}}>
+        <h1>Hello, World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
