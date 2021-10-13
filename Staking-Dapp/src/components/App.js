2a7db2e9c8574e1d93b1423a18b72fa9c7be2b37
a daana <notaliald1@gmail.com>
2021-10-13 21:06:09 +0300

    load address from the block chain and connect to MetaMask using we3

--- Staking-Dapp/src/components/App.js
+++ Staking-Dapp/src/components/App.js
@@ -2,9 +2,38 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import Web3 from "web3";
 
 // create class for component
 class App extends Component {
+  // calls load web3 before rendering
+  async UNSAFE_componentWillMount() {
+    // calling load web3
+    await this.loadWeb3();
+    // loading blochain Data
+    await this.loadBlockchainData();
+  }
+  // web3 detect metamask when loading page
+  async loadWeb3() {
+    // if we detect metamask
+    if (window.ethereum) {
+      // create new instance for web3
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      // if we find web3 then we go with the provider
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("No ethereum browser detected, example: MetaMast");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const account = web3.eth.getAccounts();
+    console.log(account);
+  }
+
   constructor(props) {
     super(props);
     // innitializing state
