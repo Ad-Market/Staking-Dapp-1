d539e4e6d128ca36e1cd8e07a495eee116a29d91
a daana <notaliald1@gmail.com>
2021-10-13 03:08:56 +0300

    testing decentral bank transfers and deployment

--- Staking-Dapp/test/decentralBank.tests.js
+++ Staking-Dapp/test/decentralBank.tests.js
@@ -9,12 +9,32 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("DecentralBank", (accounts) => {
+contract("DecentralBank", ([owner, customer]) => {
   // code for testing
   // running assersions to vheck if contract deployed has the same name
+  let tether, rwd, decentralBank;
+
+  function tokens(number) {
+    return web3.utils.toWei(number, "ether");
+  }
+
+  before(async () => {
+    // loading contracts
+    tether = await Tether.new();
+    rwd = await RWD.new();
+
+    // checking if the adresses are coming through
+    decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+    // check if the rewards are transfered to decentral bank 1 million
+    await rwd.transfer(decentralBank.address, tokens("1000000"));
+
+    // check if 100 tokens sent to the second address
+    await tether.transfer(customer, tokens("100"), { from: owner });
+  });
+
   describe("Mock tether deployment", async () => {
     it("Matches name successfuly", async () => {
-      let tether = await Tether.new();
       const name = await tether.name();
       assert.equal(name, "Mock Tether Token");
     });
@@ -22,8 +42,7 @@ contract("DecentralBank", (accounts) => {
 
   describe("Reward Token deployment", async () => {
     it("Matches name successfuly", async () => {
-      let reward = await RWD.new();
-      const name = await reward.name();
+      const name = await rwd.name();
       assert.equal(name, "Reward Token");
     });
   });
