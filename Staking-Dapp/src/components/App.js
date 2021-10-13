890d9417a7af7108bc9b1c6c0abec8288f5aad58
a daana <notaliald1@gmail.com>
2021-10-13 20:50:53 +0300

    innitialize state for account number

--- Staking-Dapp/src/components/App.js
+++ Staking-Dapp/src/components/App.js
@@ -5,10 +5,18 @@ import Navbar from "./Navbar";
 
 // create class for component
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // innitializing state
+    this.state = {
+      account: "0x0",
+    };
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar account={this.state.account} />
         <div className="text-center">
           <h1>Hello, World</h1>
         </div>
